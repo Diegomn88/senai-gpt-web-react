eea9714c1ee27e779ffd6b70d8c7f449637e736f
DIEGOMNOBREGA@YAHOO.COM.BR <fic@info.local>
2025-04-15 20:49:59 -0300

    criação projeto react

---
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -0,0 +1,20 @@
+import "./login.css";
+
+function login() {
+   
+
+
+    // JavaScript aqui.
+  
+  
+    return (
+      <>
+  
+      <h1>Login</h1>
+  
+    
+      </>
+    )
+  }
+  
+  export default login
